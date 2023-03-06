f2e5921888cd5aa408d3b7f8b05a1d4135cd9c29
QueenChristina <qinkristina@gmail.com>
2023-03-05 22:01:35 -0800

    Add movable axis by buttons in Serial Communications printer connection dialog

--- lorien/addons/GDSerCommDock/MenuLogic.gd
+++ lorien/addons/GDSerCommDock/MenuLogic.gd
@@ -23,11 +23,13 @@ onready var endlineCheck = $"MarginContainer/HBoxContainer/VBoxContainer/SendVBo
 onready var readLabel = $"MarginContainer/HBoxContainer/VBoxContainer/SerialReadRichTextLabel"
 # Printer specific
 onready var gcodeTextEdit = $"MarginContainer/HBoxContainer/VBoxContainer2/TextEdit"
-
+onready var axes_options = $"MarginContainer/HBoxContainer/VBoxContainer2/HBoxContainer/VBoxContainer/HBoxContainer/VAxis"
+var move_amt = 0.01
 
 func _ready():
 	#adding the port and baudrate options
 	_on_RefreshButton_pressed()
+	move_amt = $"MarginContainer/HBoxContainer/VBoxContainer2/HBoxContainer/VBoxContainer2/MoveAmount".value
 
 #On refreash button pressed update PORT lists
 func _on_RefreshButton_pressed():
@@ -56,18 +58,23 @@ func _on_RefreshButton_pressed():
 func _physics_process(delta):
 	if PORT != null && PORT.get_available() > 0:
 		for i in range(PORT.get_available()):
-			readLabel.add_text(str(PORT.read()))
+#			readLabel.add_text(str(PORT.read()))
+			readLabel.append_bbcode("[color=#ffffff]" + str(PORT.read()) + "[/color]")
 
 func send_text():
-	#LineEdit does not recognize endline
-	var text=sendLine.text.replace(("\\n"),com.endline)
+	if port != null and PORT != null:
+		#LineEdit does not recognize endline
+		var text=sendLine.text.replace(("\\n"),com.endline)
 
-	if endlineCheck.pressed: #if checkbox is active, add endline
-		text+=com.endline
+		if endlineCheck.pressed: #if checkbox is active, add endline
+			text+=com.endline
 
-	PORT.write(text) #write function, please use only ascii
-	sendLine.text=""
-
+		PORT.write(text) #write function, please use only ascii
+		sendLine.text=""
+		readLabel.append_bbcode("[color=#c4c4c4]Sent: " + text + "[/color]")
+	else:
+		readLabel.append_bbcode("[color=#ffffff]No port selected. Cannot send.[/color]")
+		
 func _on_PortOptionButton_item_selected(ID):
 	port = portButton.get_item_text(ID)
 
@@ -115,9 +122,60 @@ func _on_SendTextEdit_pressed():
 	send(gcodeTextEdit.text)
 
 func send(text):
-	if endlineCheck.pressed: #if checkbox is active, add endline
-		text+=com.endline
-	PORT.write(text) #write function, please use only ascii
+	if port != null and PORT != null:
+		if endlineCheck.pressed: #if checkbox is active, add endline
+			text+=com.endline
+		PORT.write(text) #write function, please use only ascii
+		readLabel.append_bbcode("[color=#c4c4c4]Sent: " + text + "[/color]")
+	else:
+		print_debug("No active port selected. Port is null.")
+		readLabel.append_bbcode("[color=#ffffff]No port selected. Cannot send.[/color]")
 
 func _on_CopyText_pressed():
 	OS.set_clipboard(gcodeTextEdit.text)
+
+func _get_jog_speed():
+	var printer_settings = Settings.get_value(Settings.PRINTER_SETTINGS, Config.DEFAULT_PRINTER_SETTINGS)
+	var curr_printer =  Settings.get_value(Settings.CURR_PRINTER_NAME, Config.DEFAULT_CURR_PRINTER_NAME)
+	var curr_printer_settings = printer_settings[curr_printer]
+
+	return curr_printer_settings.jog_speed
+	
+func _get_axes():
+	var printer_settings = Settings.get_value(Settings.PRINTER_SETTINGS, Config.DEFAULT_PRINTER_SETTINGS)
+	var curr_printer =  Settings.get_value(Settings.CURR_PRINTER_NAME, Config.DEFAULT_CURR_PRINTER_NAME)
+	var curr_printer_settings = printer_settings[curr_printer]
+
+	return curr_printer_settings.axis_order
+	
+func _on_YPos_pressed():
+	send("G0 Y%s %s" % [move_amt, _get_jog_speed()])
+
+func _on_XNeg_pressed():
+	send("G0 X-%s %s" % [move_amt, _get_jog_speed()])
+
+func _on_XPos_pressed():
+	send("G0 X%s %s" % [move_amt, _get_jog_speed()])
+
+func _on_YNeg_pressed():
+	send("G0 Y-%s %s" % [move_amt, _get_jog_speed()])
+
+func _on_ZPos_pressed():
+	send("G0 %s%s %s" % [axes_options.text, move_amt, _get_jog_speed()])
+
+func _on_ZNeg_pressed():
+	send("G0 %s-%s %s" % [axes_options.text, move_amt, _get_jog_speed()])
+
+func _on_MoveAmount_value_changed(value):
+	move_amt = value
+
+func _on_SerialCommunication_about_to_show():
+	set_axes()
+	
+func set_axes():
+	axes_options.clear()
+	for axis in _get_axes():
+		axes_options.add_item(axis)
+	if axes_options.get_item_count() <= 0:
+		axes_options.add_item("Z")
+	axes_options.select(0)
